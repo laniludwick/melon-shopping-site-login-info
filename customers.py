dd274e34ab1621df7dc89388844738dae1ee6893
Glorili Alejandro <gloriliale@gmail.com>
2020-07-23 17:59:31 -0700

    completed customers.py

--- customers.py
+++ customers.py
@@ -4,4 +4,53 @@
 class Customer(object):
     """Ubermelon customer."""
 
-    # TODO: need to implement this
+    def __init__(self, first_name, last_name, email, password):
+        """Creates a customer"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
+
+    def __repr__(self):
+        return (f"< Customer:{self.first_name} {self.last_name}, email: {self.email}>, password: {self.password}")
+
+def read_customers_from_file(filepath):
+    """Read customer data and populate dictionary of customers.
+
+    Dictionary will be {email: Customer Object}
+    """
+    customers = {}
+
+    with open(filepath) as file:
+        for line in file:
+            (first_name,
+             last_name,
+             email,
+             password,
+             ) = line.strip().split("|")
+
+    
+            customers[email] = Customer(first_name,
+                                          last_name,
+                                          email,
+                                          password,)
+
+    return customers
+
+
+def get_by_email(email):
+    return customers[email]
+
+
+customers = read_customers_from_file('customers.txt')
+
+
+
+
+
+
+
+
+
+
+
